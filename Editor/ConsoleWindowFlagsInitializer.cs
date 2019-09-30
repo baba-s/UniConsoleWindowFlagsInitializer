29e17e5b1cf7b8231cc16d0c64661cbbdcdaf20d
baba_s <shotababa1206@gmail.com>
2019-09-30 15:52:35 +0900

    first commit

---
+++ Editor/ConsoleWindowFlagsInitializer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+
+namespace KoganeUnityLib
+{
+	internal static class ConsoleWindowUtils
+	{
+		public enum ConsoleFlags
+		{
+			Collapse        = 1 << 0,
+			ClearOnPlay     = 1 << 1,
+			ErrorPause      = 1 << 2,
+			Verbose         = 1 << 3,
+			StopForAssert   = 1 << 4,
+			StopForError    = 1 << 5,
+			Autoscroll      = 1 << 6,
+			LogLevelLog     = 1 << 7,
+			LogLevelWarning = 1 << 8,
+			LogLevelError   = 1 << 9,
+			ShowTimestamp   = 1 << 10,
+			ClearOnBuild    = 1 << 11,
+		}
+	}
+
+	internal static class LogEntriesUtils
+	{
+		private const BindingFlags BINDING_ATTR = BindingFlags.Static | BindingFlags.Public;
+
+		private static readonly Assembly   m_assembly = typeof( AssetDatabase ).Assembly;
+		private static readonly Type       m_type     = m_assembly.GetType( "UnityEditor.LogEntries" );
+		private static readonly MethodInfo m_info     = m_type.GetMethod( "SetConsoleFlag", BINDING_ATTR );
+
+		public static void SetConsoleFlag( ConsoleWindowUtils.ConsoleFlags bit, bool value )
+		{
+			m_info.Invoke( null, new object[] { ( int ) bit, value } );
+		}
+	}
+
+	[InitializeOnLoad]
+	internal static class ConsoleWindowFlagsInitializer
+	{
+		static ConsoleWindowFlagsInitializer()
+		{
+			LogEntriesUtils.SetConsoleFlag( ConsoleWindowUtils.ConsoleFlags.ClearOnPlay, true );
+			LogEntriesUtils.SetConsoleFlag( ConsoleWindowUtils.ConsoleFlags.ShowTimestamp, true );
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Uni Console Window Flags Initializer
+
+Unity 起動時や再生時に Console ウィンドウの Clear on Play と Show Timestamp をオンにするエディタ拡張
+
+![](https://img.shields.io/badge/Unity-2018.4%2B-red.svg)
+![](https://img.shields.io/badge/.NET-4.x-orange.svg)
+[![](https://img.shields.io/github/license/baba-s/uni-console-window-flags-initializer.svg)](https://github.com/baba-s/uni-console-window-flags-initializer/blob/master/LICENSE)
+
+## インストール
+
+```json
+"com.baba_s.uni-console-window-flags-initializer": "https://github.com/baba-s/uni-console-window-flags-initializer.git",
+```
+
+manifest.json に上記の記述を追加します  
